b8e0d2cb2dd2305984b971912121405da40ec7b1
Bonghui(Nora) <74698994+Nora1476@users.noreply.github.com>
2024-01-24 00:30:03 +0900

    240124

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,9 +1,16 @@
 import { atom, selector } from "recoil";
 
+//enum을 지정해줌으로써 설정시 생기는 오류로부터 보호받을 수 있음
+export enum Categories {
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
+}
+
 export interface IToDo {
   text: string;
   id: number;
-  category: "TO_DO" | "DOING" | "DONE";
+  category: Categories;
 }
 
 export const toDoState = atom<IToDo[]>({
@@ -11,9 +18,9 @@ export const toDoState = atom<IToDo[]>({
   default: [],
 });
 
-export const categoryState = atom({
+export const categoryState = atom<Categories>({
   key: "category",
-  default: "TO_DO",
+  default: Categories.TO_DO,
 });
 
 //selector는 key값을 가지며 get이라는 매서드로 다른 atom을 불러와 원본을 훼손하지 않고 가공
