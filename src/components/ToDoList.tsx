2c295915afb697ca0075fb0e7cc4f11dce8503e4
Nora1476 <74698994+Nora1476@users.noreply.github.com>
2024-01-23 23:14:29 +0900

    240123

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,12 +1,13 @@
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo";
-import { toDoState } from "../atoms";
+import { toDoSelector, toDoState } from "../atoms";
 import ToDo from "./Todo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-
-  console.log(toDos);
+  const toDos = useRecoilValue(toDoState); //atom내 toDoState객체 값을 불러오기만 가능
+  const selectorOutput = useRecoilValue(toDoSelector);
+  console.log(selectorOutput);
+  // console.log(toDos);
   return (
     <div>
       <h1>To Dos</h1>
@@ -15,7 +16,7 @@ function ToDoList() {
       <ul>
         {toDos.map((toDo) => (
           // <ToDo text={toDo.text} category={toDo.category} id={toDo.id} />
-          <ToDo key={toDo.id} {...toDo} />
+          <ToDo key={toDo.id} {...toDo} /> //위 코드와 같은 코드이며 atom파일 내 객제와 ToDo타입이 완전히 일치해서 ...사용가능
         ))}
       </ul>
     </div>
